9a62af5046428e0691f7620a364797655b05be74
SWRevo <sketchdevid@gmail.com>
2021-05-26 16:07:31 +0800

    Update Source Api

--- app/src/main/java/id/indosw/digipanel/ConfigApi.java
+++ app/src/main/java/id/indosw/digipanel/ConfigApi.java
@@ -2,8 +2,8 @@ package id.indosw.digipanel;
 
 public class ConfigApi {
 	public static String SERVER_H2H = "https://api.digiflazz.com/v1/";
-    public static String USERNAME = "puxokag4EVGo";
-    public static String KEY = "35b91b44-baac-5d5d-a6ae-d7de41180abb";
+    public static String USERNAME = "YOUR_USERNAME";
+    public static String KEY = "YOUR_KEY";
     public static String BACKEND_URL = "https://kopimanis.my.id/digiflazz/backend_api.php";
     public static String BACKEND_URL_CEKSALDO = "https://kopimanis.my.id/digiflazz/api-ceksaldo.php";
     public static String BACKEND_URL_PRICELIST = "https://kopimanis.my.id/digiflazz/api-productlist.php";
