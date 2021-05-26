10b5af9c520df8bc0895625cfceddb39588faa7e
SWRevo <sketchdevid@gmail.com>
2021-05-26 16:09:34 +0800

    Update Source Api

--- app/src/main/java/id/indosw/digipanel/ConfigApi.java
+++ app/src/main/java/id/indosw/digipanel/ConfigApi.java
@@ -4,9 +4,9 @@ public class ConfigApi {
 	public static String SERVER_H2H = "https://api.digiflazz.com/v1/";
     public static String USERNAME = "YOUR_USERNAME";
     public static String KEY = "YOUR_KEY";
-    public static String BACKEND_URL = "https://kopimanis.my.id/digiflazz/backend_api.php";
-    public static String BACKEND_URL_CEKSALDO = "https://kopimanis.my.id/digiflazz/api-ceksaldo.php";
-    public static String BACKEND_URL_PRICELIST = "https://kopimanis.my.id/digiflazz/api-productlist.php";
-    public static String BACKEND_URL_DEPOSIT_SALDO = "https://kopimanis.my.id/digiflazz/api-depositsaldo.php";
-    public static String BACKEND_URL_TOPUP = "https://kopimanis.my.id/digiflazz/api-topupgolive.php";
+    public static String BACKEND_URL = "YOUR_BACKEND_DOMAIN_URL";
+    public static String BACKEND_URL_CEKSALDO = "YOUR_BACKEND_DOMAIN_URL";
+    public static String BACKEND_URL_PRICELIST = "YOUR_BACKEND_DOMAIN_URL";
+    public static String BACKEND_URL_DEPOSIT_SALDO = "YOUR_BACKEND_DOMAIN_URL";
+    public static String BACKEND_URL_TOPUP = "YOUR_BACKEND_DOMAIN_URL";
 }
